169d1fbdb6d3c325df21751a0fe6be970cd6b5a5
Bonggurajy <ajinsuk3@naver.com>
2022-08-05 00:54:48 +0900

    html & css 2

--- index.js
+++ index.js
@@ -14,34 +14,38 @@
 //5. guessNum이 gameNum과 동일하다면 console에 결과를 출력
 //<기능추가> 게임 도중 q입력시 또는 prompt창의 취소버튼 클릭 시 게임 중단하기
 const $currentState = document.querySelector('#currentState');
+const $startBtn = document.querySelector('#startBtn');
 
-
-let maxNum = prompt('Please Write maxNum')
-let maxNumParsed = parseInt(maxNum);
-if(maxNum !== null) {
-  while(!maxNumParsed) {
-    maxNumParsed = parseInt(prompt('Please Write valid Number'));
-  }
-
-  const gameNum = Math.floor(Math.random()*maxNumParsed) + 1;
-
-  let guessNum = prompt('Guess Number !');
-  let attemptedNum = 1;
-  while(parseInt(guessNum) !== gameNum) {
-    attemptedNum++;
-    if(guessNum === 'q' || guessNum === null) break;
-    if(parseInt(guessNum) < gameNum) {
-      guessNum = prompt('above !');
-    } 
-    else {
-      guessNum = prompt('below !');
+function game () {
+  let maxNum = prompt('Please Write maxNum')
+  let maxNumParsed = parseInt(maxNum);
+  if(maxNum !== null) {
+    while(!maxNumParsed) {
+      maxNumParsed = parseInt(prompt('Please Write valid Number'));
     }
-  }
-  if(guessNum === 'q') {
-    $currentState.innerHTML= 'Quitted !'
-  }
-  else {
-    $currentState.innerHTML= `Correct ! the answer is ${gameNum} ! Your attempt number is ${attemptedNum}`;
+
+    const gameNum = Math.floor(Math.random()*maxNumParsed) + 1;
+
+    let guessNum = prompt('Guess Number !');
+    let attemptedNum = 1;
+    while(parseInt(guessNum) !== gameNum) {
+      attemptedNum++;
+      if(guessNum === 'q' || guessNum === null) break;
+      if(parseInt(guessNum) < gameNum) {
+        guessNum = prompt('above !');
+      } 
+      else {
+        guessNum = prompt('below !');
+      }
+    }
+    if(guessNum === 'q') {
+      $currentState.innerHTML= 'Quitted !'
+    }
+    else {
+      $currentState.innerHTML= `Correct ! the answer is ${gameNum} ! Your attempt number is ${attemptedNum}`;
+    }
   }
 }
 
+
+$startBtn.addEventListener('click', () => setTimeout(game, 500));
