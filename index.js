ea4bfa2a68be6dbcce6a6334be94d35e003eeb0b
Bonggurajy <ajinsuk3@naver.com>
2022-08-04 21:17:13 +0900

    add Validation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //1. 사용자가 기준이 될 최대 숫자(maxNum)을 지정하게 할 것.
-
+//1-a maxNum에 유효한 타입의 숫자가 입력되었는가? --> validation
 //2. 0과 maxNum 사이의 임의의 숫자가 gameNum이 되도록 설계할 것.
 
 
@@ -14,21 +14,22 @@
 //5. guessNum이 gameNum과 동일하다면 console에 결과를 출력
 
 let maxNum = parseInt(prompt('Please Write maxNum'));
-const gameNum = Math.floor(Math.random()*maxNum);
+while(!maxNum) {
+  maxNum = parseInt(prompt('Please Write valid Number'));
+}
+const gameNum = Math.floor(Math.random()*maxNum) + 1;
 
 if (maxNum) {
   let guessNum = parseInt(prompt('Guess Number !'));
   let attemptedNum = 1;
   while(guessNum !== gameNum) {
+    attemptedNum++;
     if(guessNum < gameNum) {
       guessNum = prompt('above !');
-      attemptedNum++;
     } 
-    else if (guessNum > gameNum) {
+    else {
       guessNum = prompt('below !');
-      attemptedNum++;
     }
-    else break;
   }
   console.log(`Correct ! the answer is ${gameNum} ! Your attempt number is ${attemptedNum}`);
 }
